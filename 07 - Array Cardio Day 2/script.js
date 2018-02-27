a69c49e5256205c1f660481d42f60e2b099837a1
Graham Cottridge <codinggc@gmail.com>
2018-02-27 20:24:52 +0000

    Added array exercise2 solutions

--- 07 - Array Cardio Day 2/script.js
+++ 07 - Array Cardio Day 2/script.js
@@ -19,10 +19,59 @@ const comments = [
 //     Array.prototype.some() // is at least one person 19 or older?
 //     Array.prototype.every() // is everyone 19 or older?
 
+// const isAdult = people.some(function(person) {
+//   const currentYear = (new Date()).getFullYear();
+//   if(currentYear - person.year >= 19) {
+//     return true;
+//   }
+// });
+
+  //REFACTORED
+  // const isAdult = people.some(person => {
+  //   currentYear = (new Date()).getFullYear();
+  //   return currentYear - person.year >= 19;
+  // })
+
+  // REFACTORED WITH IMPLICIT RETURN
+  // const isAdult = people.some(person => ((new Date()).
+  //   getFullYear()) - person.year >= 19 );
+
+  // EVERY FUNCTION
+//   const isAdult = people.every(person => ((new Date()).
+//     getFullYear()) - person.year >= 19 );
+
+// console.log({isAdult})
+
 //     Array.prototype.find()
 //     Find is like filter, but instead returns just the one you are looking for
 //     find the comment with the ID of 823423
 
+// const comment = comments.find(function(comment) {
+//   if(comment.id === 823423) {
+//     return true;
+//   }
+// })
+
+// const comment = comments.find(comment => (comment.id === 823423))
+
+// console.log({comment})
+
 //     Array.prototype.findIndex()
 //     Find the comment with this ID
 //     delete the comment with the ID of 823423
+
+const index = comments.findIndex(comment => (comment.id === 823423))
+
+// console.log({index})
+
+// //TO DELETE
+
+// comments.splice(index, 1);
+
+//TO DELETE with new array
+
+const newComments = [
+  ...comments.splice(0, index),
+  ...comments.splice(index + 1)
+];
+
